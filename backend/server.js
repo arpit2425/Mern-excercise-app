64f01fbcd191afbededb3afadbd9cf3c954b70a8
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-12 00:40:02 +0530

    Frontend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const cors = require('cors')
-const app = express()
-const userRoute = require('./routes/userRoute')
-const excerciseRoute = require('./routes/excerciseRoute')
-require('dotenv').config()
-app.use(cors())
-app.use(express.json())
-const uri = process.env.URI
+const cors = require("cors");
+const app = express();
+const userRoute = require("./routes/userRoute");
+const excerciseRoute = require("./routes/excerciseRoute");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const uri = process.env.URI;
 
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('Database Connected'))
-app.use('/exercises', excerciseRoute)
-app.use('/users', userRoute)
-app.listen(8000, () => {
-  console.log('Server started')
-})
+  .then(() => console.log("Database Connected"));
+app.use("/exercises", excerciseRoute);
+app.use("/users", userRoute);
+app.listen(5000, () => {
+  console.log("Server started");
+});
