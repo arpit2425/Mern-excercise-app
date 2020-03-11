d95dafba0d6ac956df6907eca212f601deeef213
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-11 23:33:33 +0530

    Routes added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 
 const cors = require('cors')
 const app = express()
+const userRoute = require('./routes/userRoute')
+const excerciseRoute = require('./routes/excerciseRoute')
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
@@ -14,6 +16,8 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log('Database Connected'))
+app.use('/exercises', excerciseRoute)
+app.use('/users', userRoute)
 app.listen(8000, () => {
   console.log('Server started')
 })
